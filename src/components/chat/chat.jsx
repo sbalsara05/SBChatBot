3b23df1390c76b7468d6f2588bb6a63b05a41cec
sbalsara05 <balsarasohum@gmail.com>
2024-07-10 13:35:31 -0400

    Added emoji picker and implemented it showing during messages

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -1,5 +1,18 @@
 import "./chat.css"
+import EmojiPicker from "emoji-picker-react";
+import {useEffect, useState} from "react";
+
 const Chat = () => {
+    const[open, setOpen] = useState(false);
+    const[text, setText] = useState("");
+
+
+    const handleEmoji = e =>{
+        setText(prev=>prev+e.emoji);
+        setOpen(false);
+    }
+
+    console.log(text);
     return (
         <div className="chat">
             <div className="top">
@@ -23,9 +36,11 @@ const Chat = () => {
                     <img src="/camera.png" alt=""/>
                     <img src="/mic.png" alt=""/>
                 </div>
-                <input type = "text" placeholder="Type a message..."/>
+                <input type = "text" placeholder="Type a message..." value = {text} onChange={e=>setText(e.target.value)}/>
+
                 <div className="emoji">
-                    <img src="/emoji.png" alt="" />
+                    <img src="/emoji.png" alt="" onClick={()=>setOpen(prev=>!prev)}/>
+                    <EmojiPicker open={ open } onEmojiClick={handleEmoji}/>
                 </div>
                 <button className="sendButton">Send</button>
 
