5627513a994396f840f1488012609c46fa8ffd50
sbalsara05 <balsarasohum@gmail.com>
2024-07-10 13:56:13 -0400

    Moved messaging bar to the bottom

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -40,7 +40,9 @@ const Chat = () => {
 
                 <div className="emoji">
                     <img src="/emoji.png" alt="" onClick={()=>setOpen(prev=>!prev)}/>
-                    <EmojiPicker open={ open } onEmojiClick={handleEmoji}/>
+                    <div className="picker">
+                        <EmojiPicker open={ open } onEmojiClick={handleEmoji}/>
+                    </div>
                 </div>
                 <button className="sendButton">Send</button>
 
