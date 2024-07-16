1507495f3f9cc570e0033f20f90bc3296a4084aa
sbalsara05 <balsarasohum@gmail.com>
2024-07-16 15:09:25 -0400

    Implemented notification system

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,25 @@ import Chat from "./components/chat/chat.jsx";
 import Detail from "./components/detail/Detail.jsx";
 import List from "./components/list/List.jsx";
 import Login from "./components/login/Login.jsx";
+import Notification from "./components/notification/Notification.jsx";
 
 const App = () => {
 
-    const user = false
-  return (
-    <div className='container'>
-        {
-            user ? (
-                <>
-                    <List/>
-                    <Chat/>
-                    <Detail/>
-                </>
-            ): (<Login/>)
-        }
-
-
-    </div>
-  )
+    const user = true
+    return (
+        <div className='container'>
+            {
+                user ? (
+                    <>
+                        <List/>
+                        <Chat/>
+                        <Detail/>
+                    </>
+                ) : (<Login/>
+                )}
+            <Notification/>
+        </div>
+    )
 }
 
 export default App
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,20 +1,26 @@
 import "./login.css"
 import {useState} from "react";
+import {toast} from "react-toastify";
 
 const Login = () => {
     const [avatar, setAvatar] = useState({
-        file:null,
+        file: null,
         url: ""
     })
 
     const handleAvatar = (e) => {
-    if (e.target.files[0]) {
-      setAvatar({
-        file: e.target.files[0],
-        url: URL.createObjectURL(e.target.files[0]),
-      });
+        if (e.target.files[0]) {
+            setAvatar({
+                file: e.target.files[0],
+                url: URL.createObjectURL(e.target.files[0]),
+            });
+        }
+    };
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        // toast.success("Login Successful");
     }
-  };
     return <div className="login">
         <div className="item">
             <h2>Welcome back,</h2>
@@ -24,15 +30,15 @@ const Login = () => {
                 <button>Sign In</button>
             </form>
         </div>
-        <div className="seperator"></div>
+        <div className="separator"></div>
         <div className="item">
             <h2>Create an Account</h2>
-            <form>
+            <form onSubmit={handleLogin}>
                 <label htmlFor="file">
                     <img src={avatar.url || "/avatar.png"} alt="avatar"/>
                     Upload Image
                 </label>
-                <input type = "file"  id = "file" style={{display:"none"}} onChange={handleAvatar}/>
+                <input type="file" id="file" style={{display: "none"}} onChange={handleAvatar}/>
                 <input type="text" placeholder="Username" name="username"/>
                 <input type="text" placeholder="Email" name="email"/>
                 <input type="text" placeholder="Password" name="password"/>
